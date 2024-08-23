198b58c8f4294bfd877bd4de090c45b07889c42f
junsik2 <arc551234@gmail.com>
2024-08-23 15:14:55 +0900

    [추가]스크롤 숨기기

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// 모달창
 const exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
 const email = document.querySelector('#email');
 const dialog = document.querySelector('.subscribe_modal');
+const scrollTopButton = document.querySelector('.top-scroll-button');
 
 // 이메일 유효성 검사
 document.querySelector('.subscribe_btn').addEventListener('click', () => {
@@ -11,6 +11,7 @@ document.querySelector('.subscribe_btn').addEventListener('click', () => {
         dialog.showModal();
     }
 });
+
 // 모달 닫기
 document.querySelector('.ok_btn').addEventListener('click', () => {
     dialog.close();
@@ -20,4 +21,15 @@ document.querySelector('.ok_btn').addEventListener('click', () => {
 // 탑 스크롤
 document.querySelector('.top-scroll-button').addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+// 최상단 스크롤 버튼 감추기
+window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 100) {
+        scrollTopButton.style.display = 'block';
+    } else {
+        scrollTopButton.style.display = 'none';
+    }
 });
