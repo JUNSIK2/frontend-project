fb2cb61ab00d534fdbd10ce21e37cea2fdce0ea5
junsik2 <arc551234@gmail.com>
2024-08-26 13:57:07 +0900

    [추가] 모바일 레이아웃

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
 const email = document.querySelector('#email');
 const dialog = document.querySelector('.subscribe_modal');
 const scrollTopButton = document.querySelector('.top-scroll-button');
+const sideBar = document.querySelector('.side-bar');
 
 // 이메일 유효성 검사
 document.querySelector('.subscribe_btn').addEventListener('click', () => {
@@ -33,3 +34,13 @@ window.addEventListener('scroll', () => {
         scrollTopButton.style.display = 'none';
     }
 });
+
+// 사이드바
+document.querySelector('.side-button').addEventListener('click', () => {
+    sideBar.style.display  = 'block';
+});
+
+document.querySelector('.close-side-bar').addEventListener('click', () => {
+    sideBar.style.display  = 'none';
+});
+
