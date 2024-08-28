4179c737ffdf3644398bc4f93f7ffe57de6c6c16
junsik2 <arc551234@gmail.com>
2024-08-29 08:42:08 +0900

    [수정] readme

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # 오르미 1차 프론트엔드 프로젝트
+
+## 1. 프로젝트 목표 및 기능
+
+### 1.1 프로젝트 목표
+* 제공된 템플릿과 명세서를 활용해 웹페이지를 작성한다.
+* 프론트엔드 기술을 습득하여 웹개발자가 되기 위한 능력을 키운다.
+
+### 1.2 프로젝트 주요 기능
+* 탑 스크롤 버튼 구현
+* 스크롤이 최상단에 위치 시 탑 스크롤 버튼 감추기
+* 이메일 유효성을 검사
+* 모달 창 구현
+* 사이드 메뉴 바 구현
+* 카카오맵 API 사용
+
+
+## 2. 개발 환경 및 배포 URL
+
+### 2.1 개발 환경
+* 도구
+    * Visual Studio Code
+    * Figma
+* 개발 스택
+    * HTML
+    * CSS
+    * JavaScript
+* 배포
+    * GitHub Page
+
+### 2.2 배포 URL
+* GitHub Page : https://junsik2.github.io/
+
+
+## 3. 프로젝트 구조
+* 폴더 트리
+📦JUNSIK2.github.io <br/>
+ ┣ 📂css <br/>
+ ┃ ┣ 📜mobile.css <br/>
+ ┃ ┣ 📜reset.css <br/>
+ ┃ ┗ 📜styles.css <br/>
+ ┣ 📂images <br/>
+ ┣ 📂js <br/>
+ ┃ ┗ 📜script.js <br/>
+ ┗ 📜index.html <br/>
+
+
+## 4. 개발 일정
+<img src="./images/schedule.png" width="100%" height="100%"></img>
+
+
+## 5. 화면 구조
+|PC 화면|모바일 화면|
+|:---:|:---:|
+|<img src="./images/screenshot-pc.png" width="80%" height="80%"></img>|<img src="./images/screenshot-mobile.png" width="50%" height="50%"></img>|
+|유효성 검증 & 모달 창|탑 스크롤|
+|<img src="./images/validate-email.gif" width="70%" height="90%"></img>|<img src="./images/topscroll.gif" width="130%" height="150%"></img>|
+|사이드 바||
+|<img src="./images/sidebar.gif" width="50%" height="50%"></img>||
+
+
+## 6. 에러 및 해결
+* <form> 내부에서 'submit' 버튼 클릭 시 새로고침을 강제하는 기능을 방지하기위해 event.preventDefault() 메소드를 추가했습니다.
+
+
+## 7. 프로젝트를 진행하며 느낀점
+* 이번에 프로젝트를 진행하면서 웹 개발자로 취업을 위해 한걸음 더 발전을 했다고 생각합니다. 이전까지는 백엔드 위주로 공부하느라 프론트에 소홀한 부분이 있었는데 프론트엔드 프로젝트를 따로 진행하면서 html,css 이용한 레이아웃 작성법과 javascript 문법에 많은 공부를 하게 되었습니다. 다음 프로젝트를 진행 할 때는 이번에 제대로 활용하지 못했던 flexbox, grid를 사용하고 반응형 레이아웃도 구현해서 좀 더 완성도 있는 프로젝트를 만드는 것이 목표입니다.
--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
-const exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
-const email = document.querySelector('#email');
-const dialog = document.querySelector('.subscribe_modal');
-const scrollTopButton = document.querySelector('.top-scroll-button');
-const sideBar = document.querySelector('.side-bar');
 
 // 이메일 유효성 검사
-document.querySelector('.subscribe_btn').addEventListener('click', () => {
-    if(exptext.test(email.value) == false) {
-        alert('이메일 주소가 유효하지 않습니다.');
+const regex = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+const email = document.querySelector('#email');
+const dialog = document.querySelector('.subscribe_modal');
+
+document.querySelector('.subscribe_btn').addEventListener('click', (event) => {
+    if(regex.test(email.value) == false) {
+        alert('이메일 형식이 올바르지 않습니다.');
+        event.preventDefault(); // 새로고침 방지
     } else {
         dialog.showModal();
+        event.preventDefault();
     }
 });
+
 
 // 모달 닫기
 document.querySelector('.ok_btn').addEventListener('click', () => {
@@ -24,7 +26,10 @@ document.querySelector('.top-scroll-button').addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
+
 // 최상단 스크롤 버튼 감추기
+const scrollTopButton = document.querySelector('.top-scroll-button');
+
 window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY;
 
@@ -35,7 +40,10 @@ window.addEventListener('scroll', () => {
     }
 });
 
+
 // 사이드바
+const sideBar = document.querySelector('.side-bar');
+
 document.querySelector('.side-button').addEventListener('click', () => {
     sideBar.style.display  = 'block';
 });
@@ -44,3 +52,53 @@ document.querySelector('.close-side-bar').addEventListener('click', () => {
     sideBar.style.display  = 'none';
 });
 
+
+// 카카오맵 api
+var container = document.getElementById('map');
+var options = {
+    center: new kakao.maps.LatLng(33.442311, 126.571466), // 초기 위도, 경도 설정
+    level: 3 // 지도 확대, 축소 레벨
+};
+var map = new kakao.maps.Map(container, options);
+
+
+// 지도타입 컨트롤의 지도 또는 스카이뷰 버튼을 클릭하면 호출되어 지도타입을 바꾸는 함수입니다
+function setMapType(maptype) { 
+    var roadmapControl = document.getElementById('btnRoadmap');
+    var skyviewControl = document.getElementById('btnSkyview'); 
+    if (maptype === 'roadmap') {
+        map.setMapTypeId(kakao.maps.MapTypeId.ROADMAP);    
+        roadmapControl.className = 'selected_btn';
+        skyviewControl.className = 'btn';
+    } else {
+        map.setMapTypeId(kakao.maps.MapTypeId.HYBRID);    
+        skyviewControl.className = 'selected_btn';
+        roadmapControl.className = 'btn';
+    }
+}
+
+
+// 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+function zoomIn() {
+    map.setLevel(map.getLevel() - 1);
+}
+
+
+// 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+function zoomOut() {
+    map.setLevel(map.getLevel() + 1);
+}
+
+
+// 마커가 표시될 위치입니다 
+var markerPosition  = new kakao.maps.LatLng(33.442311, 126.571466); 
+
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
